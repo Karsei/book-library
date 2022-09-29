c2bc3a32eaedd30fa429803b440956788a4c5c72
Karsei <karseidev@gmail.com>
2022-09-29 23:47:53 +0900

    feat: book repository 추가

---
+++ api/src/test/java/com/gbook/book/repositories/BookRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.gbook.book.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookRepositoryTest {
+    @Test
+    void findById_success() {
+        // given
+
+        // when
+
+        // then
+    }
+}
